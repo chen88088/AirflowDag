b8fdec95563e3e3d2640e07074a0dee6f1e22c1f
chen88088 <chen88088@gmail.com>
2024-06-06 13:55:32 +0800

    modify firstDag

--- firstDag.py
+++ firstDag.py
@@ -14,3 +14,4 @@ with dag:
     end_task = EmptyOperator(task_id="end_task")
     first_task = BashOperator(task_id="first_task",
                               bash_command=f"echo execute time: {datetime.now()}")
+    start_task >> end_task >> first_task
