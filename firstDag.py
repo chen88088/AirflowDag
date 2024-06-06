1364a4382ebe43fe0f5e625edcf3eea8f5a170fa
chen88088 <chen88088@gmail.com>
2024-06-06 13:50:25 +0800

    add firstDag

---
+++ firstDag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+start_task = EmptyOperator(task_id="start_task")
+end_task = EmptyOperator(task_id="end_task")
+first_task = BashOperator(task_id="first_task",
+                          bash_command=f"echo execute time: {datetime.now()}")
+
+dag = DAG(dag_id="first_dag", tags=['user'], start_date=datetime.today())
+with dag:
+    start_task = EmptyOperator(task_id="start_task")
+    end_task = EmptyOperator(task_id="end_task")
+    first_task = BashOperator(task_id="first_task",
+                              bash_command=f"echo execute time: {datetime.now()}")
